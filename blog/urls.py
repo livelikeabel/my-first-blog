1ce94060b5866a0fecb22131eb0355b5eab9187f
abdl ko <esung1129@gmail.com>
2017-10-20 12:50:54 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+#장고의 메소드와 blog 애플리케이션에서 사용할 모든 views를 불러오고 있다.
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.post_list, name='post_list'), #post_list라는 이름의 view가 ^$ URL에 할당되었습니다
+]
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls), # 관리자url
+    url(r'', include('blog.urls')), # 장고는 http://127.0.0.1:8000/ 로 들어오는 모든 접속 요청을 blog.urls 로 전송해 추가 명령을 찾는다.
 ]
+#파이썬에서 정규 표현식을 작성할 때는 항상 문자열 앞에 r을 붙입니다. 이는 파이썬에게는 별 의미가 없지만, 파이썬에게 문자열에 특수 문자를 있다는 것을 알려줍니다.
