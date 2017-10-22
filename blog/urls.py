8373a1fdb431aacc42c07c4860a1d5cbd647662c
abdl ko <esung1129@gmail.com>
2017-10-22 10:04:09 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'), #post_list라는 이름의 view가 ^$ URL에 할당되었습니다
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
